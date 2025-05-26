6ab5104b8e05d7c253dc578c17751b05412d56c6
EkatrinaAK <137857957+EkatrinaAK@users.noreply.github.com>
2025-05-26 12:42:44 +0300

    order dto, film.dto, film schema, film repo

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,6 +4,10 @@ import {ConfigModule} from "@nestjs/config";
 import * as path from "node:path";
 
 import {configProvider} from "./app.config.provider";
+import { FilmsController } from './films/films.controller';
+import { FilmsService } from './films/films.service';
+import { OrderController } from './order/order.controller';
+import { OrderService } from './order/order.service';
 
 @Module({
   imports: [
@@ -12,8 +16,12 @@ import {configProvider} from "./app.config.provider";
           cache: true
       }),
       // @todo: Добавьте раздачу статических файлов из public
+  ServeStaticModule.forRoot({
+      rootPath: path.join(__dirname, '..', 'public'),
+      renderPath: '/content/afisha/',
+    }),
   ],
-  controllers: [],
-  providers: [configProvider],
+  controllers: [FilmsController, OrderController],
+  providers: [configProvider, FilmsService, OrderService],
 })
 export class AppModule {}
